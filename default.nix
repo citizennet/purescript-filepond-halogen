f3278a7d9e28945c5d4a89c5dff7552cb6dacef3
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-09-21 20:26:40 +0200

    Nixification, phase 1 (#2656)
    Introduce the underlying infrastructure for our Nixification work. 
    
    We packaged everything under `lib` (as well as the top-level ones like `core`, `domain`, `infrastructure`) using `purs-nix` and `flake-parts` (as mono repo framework). This compromise **phase 1** (part 1) of our work, and it shouldn't impact the existing development workflow. 
    
    In **phase 2**, we will expect that developers can switch over to using the Nix-based workflow once we iron out a few challenges (nix build speed, devShell conveniences, etc.)

---
+++ default.nix
@@ -0,0 +1,44 @@
+{ self, ... }: {
+  perSystem = { config, self', inputs', system, pkgs, ... }:
+
+    let
+      dependencies = with config.purs-nix.ps-pkgs; [
+        aff
+        effect
+        config.purs-nix.ps-pkgs.foreign
+        halogen
+        maybe
+        media-types
+        nullable
+        self'.packages.pre
+        uri
+        web-file
+        web-html
+      ];
+
+      npmlock2nix = pkgs.callPackages self.inputs.npmlock2nix { };
+      foreign.FilePond.node_modules =
+        (npmlock2nix.node_modules { src = ./.; }) + /node_modules;
+
+      ps = config.purs-nix.purs {
+        inherit dependencies foreign;
+        dir = ./.;
+      };
+
+    in
+
+    {
+      packages = {
+        filepond-halogen =
+          config.purs-nix.build {
+            name = "filepond-halogen";
+            src.path = ./.;
+            info = { inherit dependencies foreign; };
+          };
+      };
+
+      extra-shell-tools = [ (config.make-command { inherit ps; pkg = ./.; }) ];
+    };
+
+  flake = { };
+}
